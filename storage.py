cba46a3a181c2d9e5600ab88b3d0e8f7f198f745
Komyor <komyor09@gmail.com>
2025-07-10 11:32:28 +0500

    Adds task creation and listing
    Adds functionality to create new tasks with unique IDs and list existing tasks, including their status and creation date.

--- storage.py
+++ storage.py
@@ -13,7 +13,20 @@ def save_tasks(tasks):
     with open(FILE, "w") as f:
         json.dump(tasks, f, indent=2)
 
-def add_task(task):
+def add_task(title):
     tasks = load_tasks()
-    
+    new_id = max([t['id'] for t in tasks], default = 0) + 1
+    task = Task(id=new_id, title=title)
+    task.append(task.__dict__)
+    save_tasks(tasks)
+    print(f"✅ Добавлено: {title}")
+
+def list_tasks():
+    tasks = load_tasks()
+    if not tasks:
+        print("Пока задач нет.")
+    for t in tasks:
+        status = "✅" if ['is_done'] else "🕒"
+        print(f"{t['id']}. {status} {t['title']} (cоздано: {t['created_at']})")
         
+    
